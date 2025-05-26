c89318fba1e72f217f820faf1365e0cb57227f08
Valmiki Kothare <valmiki.kothare.vk@gmail.com>
2025-05-26 03:08:24 -0400

    Fix in application of threshold to abs_vel. Refactor for added clarity Improved documentation

--- src/mswaa/scroll_accelerator.py
+++ src/mswaa/scroll_accelerator.py
@@ -9,6 +9,17 @@ from .vec2 import Vec2
 
 __all__ = ("ScrollEvent", "ScrollAccelerator")
 
+logger = logging.getLogger(__name__)
+
+
+def check_config(multiplier: float, exp: float, threshold: float):
+    if multiplier <= 0:
+        raise ValueError("Multiplier must be greater than 0.")
+    if exp < 0:
+        raise ValueError("Exp must be greater than or equal to 0.")
+    if threshold < 0:
+        raise ValueError("Threshold must be greater than or equal to 0.")
+
 
 class ScrollEvent:
     def __init__(self, pos: Vec2, delta: Vec2, generated: bool):
@@ -19,6 +30,14 @@ class ScrollEvent:
 
 
 class ScrollAccelerator:
+    """Scroll accelerator.
+
+    This class uses the pynput library to listen for scroll events and
+    accelerate them. It does this by estimating the user's scroll velocity
+    and then sending additional scroll events to reach the target velocity,
+    as calculated by the acceleration parameters.
+    """
+
     _DiscreteScrollEvents = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     _VelocityEstimateMaxDeltaTime = 1.0
     _MaxScrollDelta = 100 if sys.platform != "darwin" else 1000
@@ -29,92 +48,43 @@ class ScrollAccelerator:
         exp: float = 0.0,
         threshold: float = 0.0,
     ):
-        if multiplier <= 1.0 and exp <= 0.0:
-            logging.warning(
-                f"Not using acceleration with multiplier {multiplier} and exp {exp}"
-            )
-        elif sys.platform == "darwin":
-            logging.warning(
-                "On Darwin/MacOSX, the OS already does scroll acceleration. "
-                "You can use this tool in combination, but you might want to use lower values."
-            )
-        self.accel_factor = multiplier
-        self.accel_factor_exp = exp
-        self.vel_threshold = threshold
-        self.mouse = Controller()
-        self.listener = Listener(on_scroll=self._on_scroll)
-        self._scroll_events = []  # type: list[ScrollEvent]
+        """Initialize the scroll accelerator.
+
+        Args:
+            multiplier: The linear acceleration factor.
+            exp: The exponential acceleration factor.
+            threshold: The minimum scroll speed at which the acceleration starts.
+
+        For more details, try `scroll-accelerator --help` or see the README.md
+        on GitHub (https://github.com/albertz/mouse-scroll-wheel-acceleration-userspace).
+        """
+        check_config(multiplier, exp, threshold)
+
+        self.multiplier = multiplier
+        self.exp = exp
+        self.threshold = threshold
+        self._mouse = Controller()
+        self._listener = Listener(on_scroll=self._on_scroll)
+        self._scroll_events: list[ScrollEvent] = []
         self._outstanding_generated_scrolls = Vec2()
-        self._discrete_scroll_events = sys.platform.startswith(
-            "linux"
-        )  # whether (sent) scroll events are always discrete
+        self._discrete_scroll_events = "linux" in sys.platform
+
+        logger.info(
+            f"ScrollAccelerator initialized with config: "
+            f"multiplier={multiplier}, exp={exp}, threshold={threshold}"
+        )
 
     def join(self):
-        self.listener.start()
-        self.listener.join()
+        """Join the listener thread.
 
-    def _scroll(self, delta: Vec2):
-        delta = delta.abs_cap(self._MaxScrollDelta)
-        delta = delta.round()  # in any case, backends anyway use int
-        if not delta:
-            return
-        if (
-            self._outstanding_generated_scrolls
-            and self._outstanding_generated_scrolls.sign() != delta.sign()
-        ):
-            # Don't generate a new scroll if there is an outstanding, which was in another direction.
-            return
-        self._outstanding_generated_scrolls += delta
-        self.mouse.scroll(delta.x, delta.y)
+        This will block the current thread until the listener thread is stopped.
+        """
+        self._listener.start()
+        self._listener.join()
 
-    def _on_scroll(self, x: int, y: int, dx: int, dy: int):
-        # We get user events and also generated events here.
-        # We kept track of how much scroll events we generated,
-        # which allows us to estimate the user scroll velocity.
-        pos = Vec2(x, y)
-        delta = Vec2(dx, dy)
-        generated = False
-        if delta.sign() == self._outstanding_generated_scrolls.sign():
-            generated = True
-        if self._discrete_scroll_events and delta not in self._DiscreteScrollEvents:
-            generated = False
-        if generated:
-            new_outstanding = self._outstanding_generated_scrolls - delta
-            if new_outstanding.sign() != self._outstanding_generated_scrolls.sign():
-                new_outstanding = Vec2()
-            self._outstanding_generated_scrolls = new_outstanding
-        self._scroll_events.append(ScrollEvent(pos, delta, generated=generated))
-        vel, gen_vel = self._estimate_current_scroll_velocity(
-            self._scroll_events[-1].time
-        )
-        cur_vel = vel + gen_vel
-        abs_vel = vel.l2() - self.vel_threshold
-        abs_vel_cur = cur_vel.l2()
-        logging.debug(
-            f"on scroll {(x, y)} {(dx, dy)}, gen {generated}, outstanding gen {self._outstanding_generated_scrolls},"
-            f" user vel {abs_vel}, cur vel {abs_vel_cur}"
-        )
-        # accelerate
-        m = self._acceleration_scheme_get_scroll_multiplier(abs_vel)
-        if m > 1 and abs_vel * m > abs_vel_cur:
-            # Amount of scrolling to add to get to target speed.
-            scroll_ = vel * m - cur_vel
-            (logging.debug if generated else logging.info)(
-                f"scroll user vel {abs_vel:.2f}"
-                f" -> accel multiplier {m:.2f}, cur vel {cur_vel}, target vel {abs_vel * m:.2f}"
-                f" -> scroll {scroll_}"
-            )
-            if self._discrete_scroll_events:
-                time.sleep(
-                    1e-4
-                )  # enforce some minimal sleep time before the next generated scroll
-            if self._discrete_scroll_events and scroll_.round():
-                # Scroll only by one. Once we get the next scroll event from that, we will again trigger the next.
-                self._scroll(scroll_.round().sign())
-            else:
-                self._scroll(scroll_)
-
-    def _estimate_current_scroll_velocity(self, cur_time: float) -> Tuple[Vec2, Vec2]:
+    def _estimate_current_scroll_velocity(
+        self, cur_time: float
+    ) -> Tuple[Vec2, Vec2]:
         """
         We estimate the user speed, excluding generated scroll events,
         and separately only the generated scroll events.
@@ -146,12 +116,92 @@ class ScrollAccelerator:
             f = 1  # do not increase the estimate
         return d * f, gen * f
 
-    def _acceleration_scheme_get_scroll_multiplier(
-        self, abs_vel: float
-    ) -> Union[int, float]:
-        if abs_vel <= 1:
+    def _get_scroll_multiplier(self, speed: float) -> Union[int, float]:
+        """Exponential acceleration scheme."""
+        speed_thresh = speed - self.threshold
+        if speed_thresh <= 1:
             return 1
+        return (speed_thresh**self.exp) * self.multiplier
 
-        # Exponential scheme.
-        m = abs_vel**self.accel_factor_exp
-        return m * self.accel_factor
+    def _scroll(self, delta: Vec2):
+        """Scroll by the given delta.
+
+        Args:
+            delta: The delta to scroll by.
+        """
+        delta = delta.abs_cap(self._MaxScrollDelta)
+        delta = delta.round()  # in any case, backends anyway use int
+        if not delta:
+            return
+        if (
+            self._outstanding_generated_scrolls
+            and self._outstanding_generated_scrolls.sign() != delta.sign()
+        ):
+            # Don't generate a new scroll if there is an outstanding, which was in another direction.
+            return
+        self._outstanding_generated_scrolls += delta
+        self._mouse.scroll(delta.x, delta.y)
+
+    def _on_scroll(self, x: int, y: int, dx: int, dy: int):
+        """Called when a scroll event is detected.
+
+        We get user events and also generated events here.
+        We keep track of how much scroll events we generated,
+        which allows us to estimate the user scroll velocity.
+        """
+        pos = Vec2(x, y)
+        delta = Vec2(dx, dy)
+        generated = False
+        if delta.sign() == self._outstanding_generated_scrolls.sign():
+            generated = True
+        if (
+            self._discrete_scroll_events
+            and delta not in self._DiscreteScrollEvents
+        ):
+            generated = False
+        if generated:
+            new_outstanding = self._outstanding_generated_scrolls - delta
+            if (
+                new_outstanding.sign()
+                != self._outstanding_generated_scrolls.sign()
+            ):
+                new_outstanding = Vec2()
+            self._outstanding_generated_scrolls = new_outstanding
+        self._scroll_events.append(
+            ScrollEvent(pos, delta, generated=generated)
+        )
+        user_vel, gen_vel = self._estimate_current_scroll_velocity(
+            self._scroll_events[-1].time
+        )
+        cur_vel = user_vel + gen_vel
+        user_speed = user_vel.l2()
+        cur_speed = cur_vel.l2()
+        logger.debug(
+            f"On scroll {(x, y)} {(dx, dy)}, gen {generated}, "
+            f"outstanding gen {self._outstanding_generated_scrolls}, "
+            f"user speed {user_speed}, cur speed {cur_speed}"
+        )
+
+        # Accelerate
+        m = self._get_scroll_multiplier(user_speed)
+        if m > 1 and user_speed * m > cur_speed:
+            # Amount of scrolling to add to get to target velocity.
+            scroll_delta = user_vel * m - cur_vel
+            (logger.debug if generated else logger.info)(
+                f"scroll user speed {user_speed:.2f}"
+                f" -> accel factor {m:.2f}, cur speed {cur_speed}, target speed {user_speed * m:.2f}"
+                f" -> scroll {scroll_delta}"
+            )
+
+            if self._discrete_scroll_events:
+                # Enforce some minimal sleep time before the next generated scroll
+                time.sleep(1e-4)
+
+                # Scroll only by one.
+                # Once we get the next scroll event from that, we will again trigger the next.
+                if scroll_delta.round():
+                    self._scroll(scroll_delta.round().sign())
+                    return
+
+            # Scroll by the amount of the delta.
+            self._scroll(scroll_delta)
